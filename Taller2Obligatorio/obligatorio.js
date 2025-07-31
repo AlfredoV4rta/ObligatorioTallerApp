dd7982a4bbadc0b3d63b6bcb6bd701e03f73b946
AlfredoV4rta <alfredo.vartabedian2@gmail.com>
2025-07-31 08:54:19 -0300

    Actualizacion 5

--- Taller2Obligatorio/obligatorio.js
+++ Taller2Obligatorio/obligatorio.js
@@ -1,281 +1,286 @@
-let usuarioConectado = null
-let listaPaises=[]
+let usuarioConectado = null;
+let listaPaises = [];
 
-class Usuario{
-    constructor(usuario, password, pais){
-    this.usuario = usuario 
-    this.password = password 
-    this.pais = pais
+class Usuario {
+    constructor(usuario, password, pais) {
+        this.usuario = usuario;
+        this.password = password;
+        this.pais = pais;
     }
 }
 
 class Evaluacion {
     constructor(objetivo, usuario, password, pais) {
-        this.objetivo = objetivo
-        this.usuario = usuario
-        this.password = password
-        this.pais = pais
+        this.objetivo = objetivo;
+        this.usuario = usuario;
+        this.password = password;
+        this.pais = pais;
     }
 }
 
-const Menu = document.querySelector("#menuPrincipal")
-const Ruteo = document.querySelector("#ruteo")
-const PantallaHome = document.querySelector("#pantallaHome")
-const PantallaLogin = document.querySelector("#pantallaLogin")
-const PantallaRegistrarUsuario = document.querySelector("#pantallaRegistrarUsuario")
-const PantallaRegistrarEvaluacion = document.querySelector("#pantallaRegistrarEvaluacion")
+const Menu = document.querySelector("#menuPrincipal");
+const Ruteo = document.querySelector("#ruteo");
+const PantallaHome = document.querySelector("#pantallaHome");
+const PantallaLogin = document.querySelector("#pantallaLogin");
+const PantallaRegistrarUsuario = document.querySelector(
+    "#pantallaRegistrarUsuario"
+);
+const PantallaRegistrarEvaluacion = document.querySelector(
+    "#pantallaRegistrarEvaluacion"
+);
 
-inicio()
+inicio();
 
-function inicio(){
-    ocultarTodoMenu()
-    ocultarPantalla()
-    obtenerPaises()
-    
-    PantallaHome.style.display="none"
-        if (localStorage.getItem("token")!=null){
-         mostrarMenuVIP()
-        } else {
-          mostrarMenuBasico()
-        }
+function inicio() {
+    ocultarTodoMenu();
+    ocultarPantalla();
+    obtenerPaises();
 
-   
-    Ruteo.addEventListener("ionRouteDidChange", navegar)
-    document.querySelector("#btnRegistrarUsuario").addEventListener("click", previaRegistrarUsuario)
-    document.querySelector("#btnLogin").addEventListener("click", previaLogin)
-    document.querySelector("#btnRegistrarEvaluacion").addEventListener("click", previaRegistrarEvaluacion)
-    document.querySelector("#btnMenuRegistrarEvaluacion").addEventListener("click", obtenerObjetivos)
-}
-
-function ocultarPantalla(){
-    PantallaHome.style.display="none"
-    PantallaLogin.style.display="none"
-    PantallaRegistrarUsuario.style.display="none"
-    PantallaRegistrarEvaluacion.style.display = "none"
-}
-
-function navegar(evento){
-    ocultarPantalla()
-    if(evento.detail.to=="/") PantallaHome.style.display="block"
-    if(evento.detail.to=="/login") PantallaLogin.style.display="block"
-    if(evento.detail.to=="/registrarUsuario") PantallaRegistrarUsuario.style.display="block"
-    if(evento.detail.to=="/registrarEvaluacion") PantallaRegistrarEvaluacion.style.display="block"
-}
-
-function cerrarMenu(){
-    Menu.close()
-}
-
-function mostrarMenuBasico(){
-      ocultarTodoMenu()
-        document.querySelector("#btnMenuRegistrarUsuario").style.display="block"
-        document.querySelector("#btnMenuLogin").style.display="block"
-
-}
-
-function mostrarMenuVIP(){
-      ocultarTodoMenu()
-    PantallaHome.style.display = "block"
-    document.querySelector("#btnMenuRegistrarEvaluacion").style.display="block"
-    document.querySelector("#btnMenuListaEvaluacion").style.display="block"
-    document.querySelector("#btnMenuInforme").style.display="block"
-    document.querySelector("#btnMenuMapa").style.display="block"
-    document.querySelector("#btnMenuLogout").style.display="block"
-        
-}
-
-function ocultarTodoMenu(){
-    document.querySelector("#btnMenuRegistrarUsuario").style.display="none"
-    document.querySelector("#btnMenuLogin").style.display="none"
-    document.querySelector("#btnMenuRegistrarEvaluacion").style.display="none"
-    document.querySelector("#btnMenuListaEvaluacion").style.display="none"
-    document.querySelector("#btnMenuInforme").style.display="none"
-    document.querySelector("#btnMenuMapa").style.display="none"
-    document.querySelector("#btnMenuLogout").style.display = "none"
-    
-   
-}
-
-function previaRegistrarEvaluacion(){
-    let objetivo = document.querySelector("#slcObjetivos").value
-    let calificacion = document.querySelector("#nCalificacion").value
-    let fecha = document.querySelector("#dtFecha").value
-
-    if(calificacion <=5 && calificacion >= -5) {
-        let nuevaEvaluacion = new Evaluacion(objetivo, localStorage.getItem('id'), calificacion, fecha)
-        registrarEvaluacion(nuevaEvaluacion)
-    }else {
-        alert("La calificacion va desde -5 a 5")
+    PantallaHome.style.display = "none";
+    if (localStorage.getItem("token") != null) {
+        mostrarMenuVIP();
+    } else {
+        mostrarMenuBasico();
     }
 
-    obtenerObjetivos()
+    Ruteo.addEventListener("ionRouteDidChange", navegar);
+    document
+        .querySelector("#btnRegistrarUsuario")
+        .addEventListener("click", previaRegistrarUsuario);
+    document.querySelector("#btnLogin").addEventListener("click", previaLogin);
+    document
+        .querySelector("#btnRegistrarEvaluacion")
+        .addEventListener("click", previaRegistrarEvaluacion);
+    document
+        .querySelector("#btnMenuRegistrarEvaluacion")
+        .addEventListener("click", obtenerObjetivos);
 }
 
-function previaRegistrarUsuario(){
+function ocultarPantalla() {
+    PantallaHome.style.display = "none";
+    PantallaLogin.style.display = "none";
+    PantallaRegistrarUsuario.style.display = "none";
+    PantallaRegistrarEvaluacion.style.display = "none";
+}
 
-    let usuario= document.querySelector("#txtUser").value
-    let password= document.querySelector("#txtPassword").value
-    let pais= document.querySelector("#slcPais").value
+function navegar(evento) {
+    ocultarPantalla();
+    if (evento.detail.to == "/") PantallaHome.style.display = "block";
+    if (evento.detail.to == "/login") PantallaLogin.style.display = "block";
+    if (evento.detail.to == "/registrarUsuario")
+        PantallaRegistrarUsuario.style.display = "block";
+    if (evento.detail.to == "/registrarEvaluacion")
+        PantallaRegistrarEvaluacion.style.display = "block";
+}
 
-    let nuevoUsuario= new Usuario(usuario, password, pais)
-    registrarUsuario(nuevoUsuario)
+function cerrarMenu() {
+    Menu.close();
+}
+
+function mostrarMenuBasico() {
+    ocultarTodoMenu();
+    document.querySelector("#btnMenuRegistrarUsuario").style.display = "block";
+    document.querySelector("#btnMenuLogin").style.display = "block";
+}
+
+function mostrarMenuVIP() {
+    ocultarTodoMenu();
+    PantallaHome.style.display = "block";
+    document.querySelector("#btnMenuRegistrarEvaluacion").style.display = "block";
+    document.querySelector("#btnMenuListaEvaluacion").style.display = "block";
+    document.querySelector("#btnMenuInforme").style.display = "block";
+    document.querySelector("#btnMenuMapa").style.display = "block";
+    document.querySelector("#btnMenuLogout").style.display = "block";
+}
+
+function ocultarTodoMenu() {
+    document.querySelector("#btnMenuRegistrarUsuario").style.display = "none";
+    document.querySelector("#btnMenuLogin").style.display = "none";
+    document.querySelector("#btnMenuRegistrarEvaluacion").style.display = "none";
+    document.querySelector("#btnMenuListaEvaluacion").style.display = "none";
+    document.querySelector("#btnMenuInforme").style.display = "none";
+    document.querySelector("#btnMenuMapa").style.display = "none";
+    document.querySelector("#btnMenuLogout").style.display = "none";
+}
+
+function previaRegistrarEvaluacion() {
+    let objetivo = document.querySelector("#slcObjetivos").value;
+    let calificacion = document.querySelector("#nCalificacion").value;
+    let fecha = document.querySelector("#dtFecha").value;
+
+    if (calificacion <= 5 && calificacion >= -5) {
+        let nuevaEvaluacion = new Evaluacion(
+            objetivo,
+            localStorage.getItem("id"),
+            calificacion,
+            fecha
+        );
+        registrarEvaluacion(nuevaEvaluacion);
+    } else {
+        alert("La calificacion va desde -5 a 5");
+    }
+
+    obtenerObjetivos();
+}
+
+function previaRegistrarUsuario() {
+    let usuario = document.querySelector("#txtUser").value;
+    let password = document.querySelector("#txtPassword").value;
+    let pais = document.querySelector("#slcPais").value;
+
+    let nuevoUsuario = new Usuario(usuario, password, pais);
+    registrarUsuario(nuevoUsuario);
 }
 
 function registrarEvaluacion(nuevaEvaluacion) {
-    fetch (`https://goalify.develotion.com/evaluaciones.php`,{
-        method:'POST',
-        headers:{
-        'Content-Type': 'application/json',
-        'token': localStorage.getItem('token'),
-        'iduser': localStorage.getItem('id')
-    },
-        body: JSON.stringify(nuevaEvaluacion)
+    fetch(`https://goalify.develotion.com/evaluaciones.php`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "token": localStorage.getItem("token"),
+            "iduser": localStorage.getItem("id"),
+        },
+        body: JSON.stringify(nuevaEvaluacion),
     })
-        .then(function (response){
-            console.log(response)
-            return response.json()
+        .then(function (response) {
+            console.log(response);
+            return response.json();
         })
-        .then(function(informacion){
-            if(informacion.error=="") {
-                alert ("Pedido registrado con éxito")
-                ocultarTodo()
-                PantallaHome.style.display = "block"
-            }
-        })
-        .catch(function(error){
-            console.log(error)
-        })
-
-}
-
-
-function registrarUsuario(nuevoUsuario){
-    fetch (`https://goalify.develotion.com/usuarios.php`,{
-        method:'POST',
-        headers:{
-        'Content-Type': 'application/json'
-    },
-        body: JSON.stringify(nuevoUsuario)
-    })
-        .then(function (response){
-            console.log(response)
-            return response.json()
-        })
-        .then(function(informacion){
+        .then(function (informacion) {
             console.log(informacion)
-            if (informacion.codigo=="200"){
-                alert ("Registro creado con éxito")
-                localStorage.setItem("token",informacion.token)
-                localStorage.setItem("id",informacion.id)
-                usuarioConectado=  informacion.id
-                ocultarPantalla()
-                PantallaHome.style.display="block"
-                mostrarMenuVIP()         
-            }
-            
+            alert(informacion.mensaje)
+            ocultarPantalla()
+            PantallaHome.style.display = "block";
         })
-        .catch(function(error){
-            console.log(error)
-        })
-
+        .catch(function (error) {
+            console.log(error);
+        });
 }
 
-function obtenerPaises(){
-     fetch("https://goalify.develotion.com/paises.php")
-        .then(function (response){
-        return response.json()
-        })
-        .then(function(informacion){ 
-          listaPaises= informacion.paises
-          cargarPaises()
-        })
-        .catch(function(error){
-        console.log(error)
-        })
-}
-
-function obtenerObjetivos(){
-    fetch("https://goalify.develotion.com/objetivos.php",{
-        method:"GET",
-        headers:{
-            'Content-Type': 'application/json',
-            'token': localStorage.getItem('token'),
-            'iduser': localStorage.getItem('id')
-        }
+function registrarUsuario(nuevoUsuario) {
+    fetch(`https://goalify.develotion.com/usuarios.php`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(nuevoUsuario),
     })
-        .then(function (response){
-        return response.json()
+        .then(function (response) {
+            console.log(response);
+            return response.json();
         })
-        .then(function(info){
+        .then(function (informacion) {
+            console.log(informacion);
+            if (informacion.codigo == "200") {
+                alert("Registro creado con éxito");
+                localStorage.setItem("token", informacion.token);
+                localStorage.setItem("id", informacion.id);
+                usuarioConectado = informacion.id;
+                ocultarPantalla();
+                PantallaHome.style.display = "block";
+                mostrarMenuVIP();
+            }
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
 
-            cargarObjetivos(info.objetivos)
+function obtenerPaises() {
+    fetch("https://goalify.develotion.com/paises.php")
+        .then(function (response) {
+            return response.json();
         })
-        .catch(function(error){
-            console.log(error)
+        .then(function (informacion) {
+            listaPaises = informacion.paises;
+            cargarPaises();
         })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
+
+function obtenerObjetivos() {
+    ocultarPantalla()
+    PantallaRegistrarEvaluacion.style.display = "block"
+    fetch("https://goalify.develotion.com/objetivos.php", {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            token: localStorage.getItem("token"),
+            iduser: localStorage.getItem("id"),
+        },
+    })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (info) {
+            cargarObjetivos(info.objetivos);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
 }
 
 function cargarObjetivos(objetivos) {
-    let miSelect = ''
+    let miSelect = "";
 
     for (let unObjetivo of objetivos) {
-        miSelect+=`<ion-select-option value=${unObjetivo.id}>${unObjetivo.nombre}</ion-select-option>`
+        miSelect += `<ion-select-option value=${unObjetivo.id}>${unObjetivo.nombre}</ion-select-option>`;
     }
 
-    document.querySelector("#slcObjetivos").innerHTML=miSelect
+    document.querySelector("#slcObjetivos").innerHTML = miSelect;
 }
 
-function cargarPaises(){
-    let miSelect=""
+function cargarPaises() {
+    let miSelect = "";
 
-    for (let unPais of listaPaises){
-        miSelect+=`<ion-select-option value=${unPais.id}>${unPais.name}</ion-select-option>`
+    for (let unPais of listaPaises) {
+        miSelect += `<ion-select-option value=${unPais.id}>${unPais.name}</ion-select-option>`;
     }
-    document.querySelector("#slcPais").innerHTML=miSelect
+    document.querySelector("#slcPais").innerHTML = miSelect;
 }
 
-function previaLogin(){
-    let usuario= document.querySelector("#txtUsuario").value  
-    let password= document.querySelector("#txtPass").value  
+function previaLogin() {
+    let usuario = document.querySelector("#txtUsuario").value;
+    let password = document.querySelector("#txtPass").value;
 
-    let nuevoLogin= new Object()
-    nuevoLogin.usuario=usuario 
-    nuevoLogin.password=password 
-    hacerLogin(nuevoLogin)
+    let nuevoLogin = new Object();
+    nuevoLogin.usuario = usuario;
+    nuevoLogin.password = password;
+    hacerLogin(nuevoLogin);
 }
 
-function hacerLogin(nuevoLogin){
-  fetch (`https://goalify.develotion.com/login.php`,{
-        method:'POST',
-        headers:{
-        'Content-Type': 'application/json'
-    },
-        body: JSON.stringify(nuevoLogin)
+function hacerLogin(nuevoLogin) {
+    fetch(`https://goalify.develotion.com/login.php`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(nuevoLogin),
     })
-        .then(function (response){
-            console.log(response)
-            return response.json()
+        .then(function (response) {
+            console.log(response);
+            return response.json();
         })
-        .then(function(informacion){
-            console.log(informacion)
-            if (informacion.codigo=="200"){
-                alert ("Bienvenido")
-                
-                localStorage.setItem("token",informacion.token)
-                localStorage.setItem("id",informacion.id)
-                usuarioConectado=  informacion.id
-                ocultarPantalla()
-                PantallaHome.style.display="block"
-                mostrarMenuVIP()
-            } else {
-            alert("Login incorrecto");
-         }
-            
-        })
-        .catch(function(error){
-            console.log(error)
-        })
+        .then(function (informacion) {
+            console.log(informacion);
+            if (informacion.codigo == "200") {
+                alert("Bienvenido");
 
+                localStorage.setItem("token", informacion.token);
+                localStorage.setItem("id", informacion.id);
+                usuarioConectado = informacion.id;
+                ocultarPantalla();
+                PantallaHome.style.display = "block";
+                mostrarMenuVIP();
+            } else {
+                alert("Login incorrecto");
+            }
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
 }
 
 function logout() {
@@ -287,15 +292,17 @@ function logout() {
     Ruteo.push("/login");
 }
 
-document.addEventListener('DOMContentLoaded', (event) => {limitarFechaActual();});
+document.addEventListener("DOMContentLoaded", (event) => {
+    limitarFechaActual();
+});
 function limitarFechaActual() {
-const hoy = new Date();
-const año = hoy.getFullYear();
-const mes = ('0' + (hoy.getMonth() + 1)).slice(-2);
-const dia = ('0' + hoy.getDate()).slice(-2);
-const horas = ('0' + hoy.getHours()).slice(-2);
-const minutos = ('0' + hoy.getMinutes()).slice(-2);
-const segundos = ('0' + hoy.getSeconds()).slice(-2);
-const fechaMaxima = `${año}-${mes}-${dia}T${horas}:${minutos}:${segundos}`;
-document.querySelector('#dtFecha').setAttribute('max', fechaMaxima);
+    const hoy = new Date();
+    const año = hoy.getFullYear();
+    const mes = ("0" + (hoy.getMonth() + 1)).slice(-2);
+    const dia = ("0" + hoy.getDate()).slice(-2);
+    const horas = ("0" + hoy.getHours()).slice(-2);
+    const minutos = ("0" + hoy.getMinutes()).slice(-2);
+    const segundos = ("0" + hoy.getSeconds()).slice(-2);
+    const fechaMaxima = `${año}-${mes}-${dia}T${horas}:${minutos}:${segundos}`;
+    document.querySelector("#dtFecha").setAttribute("max", fechaMaxima);
 }
